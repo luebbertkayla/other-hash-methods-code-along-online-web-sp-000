e604ff52c3742c0089d127fbba52e2885bcc387e
Kayla Luebbert <luebbertkayla@gmail.com>
2021-02-09 03:11:27 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -12,11 +12,5 @@ def get_the_values(groceries)
   groceries.values.flatten
 end
 
-def get_the_keys(groceries)
-  groceries.keys
-end
 
-def get_the_min(groceries)
-  groceries.values.flatten.min
-end
 
